6b87dd4b8dbbe5c9cf6e54031a8958a055a8f57f
Brent Lewis <coder0xff@gmail.com>
2012-09-02 11:51:42 -0700

    Try again

--- Alterity/Scripts/DocumentEdit/DeletionHunk.js
+++ Alterity/Scripts/DocumentEdit/DeletionHunk.js
@@ -1,1 +1,29 @@
-﻿
+﻿define(["IntegerInterval"], function () {
+
+    var DeletionHunk = function (startIndex, length) {
+        this.type = "deletion";
+        this.startIndex = startIndex;
+        this.length = length;
+    };
+
+    DeletionHunk.prototype.getIntegerInterval = function () {
+        return new IntegerInterval(startIndex, length);
+    }
+
+    DeletionHunk.prototype.MergeSubsequent = function (other) {
+        if (!other) throw "argument null";
+        if (other.type == "deletion") {
+            var otherInterval = other.getIntegerInterval();
+            if (otherInterval.left == startIndex) {
+                length += otherInterval.length;
+                return null;
+            } else {
+                return other;
+            }
+        }
+        if (other.type == "insertion") return other;
+        throw "invalid hunk type";
+    }
+
+    return DeletionHunk;
+}());
--- Alterity/Scripts/DocumentEdit/InsertionHunk.js
+++ Alterity/Scripts/DocumentEdit/InsertionHunk.js
@@ -1,1 +1,13 @@
-﻿
+﻿define(["IntegerInterval"], function () {
+    var InsertionHunk = function(startIndex, text)
+    {
+        this.startIndex = startIndex;
+        this.text = text;
+    }
+
+    Object.defineProperty(InsertionHunk.prototype, "length", {
+        get: function () { return text.length; }
+    });
+
+
+}());
