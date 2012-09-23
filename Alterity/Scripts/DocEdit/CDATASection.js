ba4fab8c300ff2a8afe9037f6221ea5239732b83
Brent Lewis <coder0xff@gmail.com>
2012-09-23 16:27:58 -0700

    DOM CDATASection

---
+++ Alterity/Scripts/DocEdit/CDATASection.js
@@ -0,0 +1,20 @@
+﻿define("Text", function (Text) {
+    function CDATASection(ownerDocument) {
+        Text.apply(this, ownerDocument);
+    }
+
+    extend(CDATASection, Text);
+
+    Object.defineProperties(CDATASection.prototype, {
+        "cloneNode": {
+            enumerable: true, value: function (deep) {
+                return ownerDocument.createComment(data);
+            }
+        },
+        "nodeName": { enumerable: true, get: function () { return "#cdata-section"; } },
+        "nodeType": { enumerable: true, get: function () { return Node.CDATA_SECTION_NODE; } },
+        "nodeValue": { enumerable: true, get: function () { return data; }, set: function (value) { data = value; } },
+    });
+
+    return CDATASection;
+});
