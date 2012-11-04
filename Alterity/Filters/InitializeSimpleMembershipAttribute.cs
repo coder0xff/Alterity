385363991aaedd8f03a7447e172c9beb022db51e
Brent <coder0xff@gmail.com>
2012-11-04 11:59:37 -0800

    Begin adding Redis use

--- Alterity/Filters/InitializeSimpleMembershipAttribute.cs
+++ Alterity/Filters/InitializeSimpleMembershipAttribute.cs
@@ -24,7 +24,6 @@ namespace Alterity.Filters
         {
             // Ensure ASP.NET Simple Membership is initialized only once per app start
             LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
-
         }
 
         private class SimpleMembershipInitializer
